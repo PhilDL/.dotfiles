467fa934d1296692947ec732a35ac3ef5700232a
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2024-09-27 21:10:15 +0400

    send CMD+S to neovim

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,13 +11,21 @@ config.window_decorations = "RESIZE"
 config.default_cursor_style = "BlinkingBlock"
 config.color_scheme = "Catppuccin Mocha"
 
--- config.window_background_opacity = 0.9
--- config.macos_window_background_blur = 10
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
 -- config.window_padding = {
 -- 	top = 0,
 -- 	bottom = 0,
 -- 	left = 3,
 -- 	right = 3,
 -- }
+
+config.keys = {
+	{
+		key = "s",
+		mods = "CMD",
+		action = wezterm.action.SendKey({ key = "s", mods = "CTRL" }),
+	},
+}
 
 return config
